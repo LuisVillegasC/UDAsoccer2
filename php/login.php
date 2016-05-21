21bae442122b2100c6ad564c2c40ce0ff957d30c
Luis Villegas <luis_ville_c_200905@hotmail.com>
2016-05-21 19:13:51 -0400

    Mains por tipo de usuario

--- php/login.php
+++ php/login.php
@@ -1,9 +1,79 @@
 <?php 
+	include('conexionBD.php');
 
+	//Activar las Sesiones en PHP
+	session_start();
 
 	$username = $_POST['username'];
 	$password = $_POST['password'];
+	$estado_cuenta = "activa";
 
-	echo $username.$password;
+	$query_condicion_where = " WHERE
+                           username = '" . $username . "'
+                           and password = '" . $password . "'" . "
+                           and estado_cuenta = '" . $estado_cuenta . "'";
+	$nombre_tabla = 'usuario';
+	$query_usuario_select = "SELECT nombre, paterno, materno, tipo_usuario FROM ".$nombre_tabla. $query_condicion_where;
+
+	echo $query_usuario_select;
+
+	try {
+
+	    $nombre = '';
+	    $paterno = '';
+	    $materno = '';
+	    $tipo_usuario = '';
+
+
+	    foreach ($conexionAlServidorBD->query($query_usuario_select) as $row) {
+	        $nombre = $row['nombre'];
+	        $paterno = $row['paterno'];
+	        $materno = $row['materno'];
+	        $tipo_usuario = $row['tipo_usuario'];
+	    }
+
+
+
+	    if (empty($nombre)) {
+	        $_SESSION['mensaje_login'] = 'Su cuenta de usuario no existe o no esta activa. Intente iniciar sesion nuevamente';
+	        header("Location: index.php");
+	    } else {
+	        // Inicializar una variable de SESION con valores
+	        $_SESSION['username']= $username;
+	        $_SESSION['password']= $password;
+	        $_SESSION['nombre']= $nombre;
+	        $_SESSION['paterno']= $paterno;
+	        $_SESSION['materno']= $materno;
+	        $_SESSION['tipo_usuario']= $tipo_usuario;
+
+
+	        if ($tipo_usuario == 'superadmin') {
+	        	header("Location: main_superadmin.php");
+	        } else if ($tipo_usuario == 'admin') {
+	        	header("Location: main_admin.php");
+	        } else if ($tipo_usuario == 'jugador') {
+	        	header("Location: main_jugador.php");
+	        } else {
+	        	//header("Location: index.html");
+	        }
+	        
+	       
+	    }
+	    $resultadoSQL = null;
+	}
+	catch (PDOException $e) {
+	    echo "¡Error!: " . $e->getMessage() . "<br/>";
+	    //Cerrar conexion
+	    $conexionAlServidorBD = null;
+	    die();
+	}
+	catch(Exception $e){
+	  echo "¡Error!: " . $e->getMessage() . "<br/>";
+	  //Cerrar conexion
+	  $conexionAlServidorBD = null;
+	  die();
+	}
+	//Cerrar conexion
+	$conexionAlServidorBD = null;
 
 ?>
---
+++ php/main_admin.php
@@ -0,0 +1,111 @@
+<?php
+  //Activar las Sesiones en PHP
+  session_start();
+  $username = '';
+  $password = '';
+  $nombre = '';
+  $paterno = '';
+  $materno = '';
+  $nombre_completo_usuario = '';
+
+  if (!empty($_SESSION['username'])) {
+      $username = $_SESSION['username'];
+  }
+  if (!empty($_SESSION['password'])) {
+      $password = $_SESSION['password'];
+  }
+  if (!empty($_SESSION['nombre'])) {
+      $nombre = $_SESSION['nombre'];
+  }
+  if (!empty($_SESSION['paterno'])) {
+      $paterno = $_SESSION['paterno'];
+  }
+  if (!empty($_SESSION['materno'])) {
+      $materno = $_SESSION['materno'];
+  }
+  if (!empty($nombre) && !empty($paterno) && !empty($materno)) {
+      $nombre_completo_usuario = $nombre.' '.$paterno.' '.$materno;
+  }else{
+    $_SESSION['mensaje_login'] = 'Su cuenta de usuario no existe o no esta activa. Intente iniciar sesion nuevamente';
+    header("Location: index.php");
+  }
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>UDAsoccer</title> <!--Nombre para la pestaña-->
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/estilos.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" class="index">
+	<!-- NAVEGACION -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                     <li class="page-scroll">
+                        <a href="#inicio">Inicio</a>
+                     </li>
+
+                     <li class="page-scroll">
+                        <a href="#nosotros">Nosotros</a>
+                     </li>
+
+                     <li class="page-scroll">
+                        <a href="#nosotros">Registrar Jugador</a>
+                     </li>                     
+
+                     <li class="page-scroll">
+                        <a href="#nosotros">Registrar Equipo</a>
+                     </li>                     
+                   
+                    <li class="page-scroll">
+            			<a href="#contact">Contacto</a>
+        			</li>
+        			<li class="page-scroll">
+            			<a href="#contact">
+            				<?php echo $nombre_completo_usuario; ?>
+
+            			</a>
+        			</li>
+                                         <li class="page-scroll">
+                  <a href="salir.php">
+                    salir                  </a>
+              </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+</body>
+</html>
