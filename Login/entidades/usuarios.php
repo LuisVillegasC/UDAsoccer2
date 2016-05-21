f31102714140ce66943e89a18ed96ff403b661af
Luis Villegas <luis_ville_c_200905@hotmail.com>
2016-05-21 18:19:32 -0400

    primer commit

---
+++ Login/entidades/usuarios.php
@@ -0,0 +1,28 @@
+<?php
+
+ class usuarios{
+	public $id;
+	function POST_id(){
+		return $this->id;
+	}
+	function set_id($id){
+		$this->id = $id;
+	}
+	public $usuario;
+	function POST_usuario(){
+		return $this->usuario;
+	}
+	function set_usuario($usuario){
+		$this->usuario = $usuario;
+	}
+	public $contrasena;
+	function POST_contrasena(){
+		return $this->contrasena;
+	}
+	function set_contrasena($contrasena){
+		$this->contrasena = $contrasena;
+	}
+ }
+
+
+?>
